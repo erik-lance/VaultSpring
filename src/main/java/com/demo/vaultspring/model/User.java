adab05965011c35d8b256f2e1c555ab7dabb1f9e
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2025-03-02 22:00:47 +0800

    Added args constructor for user

--- src/main/java/com/demo/vaultspring/model/User.java
+++ src/main/java/com/demo/vaultspring/model/User.java
@@ -3,12 +3,14 @@ package com.demo.vaultspring.model;
 import com.demo.vaultspring.model.enums.Role;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
