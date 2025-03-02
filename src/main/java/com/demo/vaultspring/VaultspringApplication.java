a11eb4ea9df143dcdaf8a702f11651483857f84b
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2025-03-02 14:13:05 +0800

    Initialized files

---
+++ src/main/java/com/demo/vaultspring/VaultspringApplication.java
@@ -0,0 +1,13 @@
+package com.demo.vaultspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/vaultspring/VaultspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.vaultspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
