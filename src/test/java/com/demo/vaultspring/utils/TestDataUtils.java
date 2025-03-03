28d955e752eaf6fd1814dbbf4fc0d1679cc7cef0
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2025-03-03 11:16:06 +0800

    Added multiple account tests

--- src/test/java/com/demo/vaultspring/utils/TestDataUtils.java
+++ src/test/java/com/demo/vaultspring/utils/TestDataUtils.java
@@ -21,6 +21,32 @@ public class TestDataUtils {
         return user;
     }
 
+    public static User USER_WITH_3_ACCOUNTS() {
+        User user = BASE_USER();
+
+        for (int i = 0; i < 3; i ++)
+        {
+            // Convert int to long for id
+            Long newId = (long) i;
+            user.addAccount(ITERABLE_ACCOUNT(newId, user));
+        }
+
+        return user;
+    }
+
+    public static User USER_WITH_X_ACCOUNTS(int x) {
+        User user = BASE_USER();
+
+        for (int i = 0; i < x; i ++)
+        {
+            // Convert int to long for id
+            Long newId = (long) i;
+            user.addAccount(ITERABLE_ACCOUNT(newId, user));
+        }
+
+        return user;
+    }
+
     public static Account BASIC_ACCOUNT() {
         Account account = new Account(1L, "0000LOCL12345678", BigDecimal.valueOf(2000), null);
         return account;
@@ -28,6 +54,20 @@ public class TestDataUtils {
 
     public static Account BASIC_ACCOUNT(User user) {
         Account account = new Account(1L, "0000LOCL12345678", BigDecimal.valueOf(2000), user);
+        return account;
+    }
+
+    public static Account ITERABLE_ACCOUNT(Long id) {
+        Long newNum = 1000L + id;
+        String accountNumber = "0000LOCL1234" + newNum.toString();
+        Account account = new Account(id, accountNumber, BigDecimal.valueOf(2000), null);
+        return account;
+    }
+
+    public static Account ITERABLE_ACCOUNT(Long id, User user) {
+        Long newNum = 1000L + id;
+        String accountNumber = "0000LOCL1234" + newNum.toString();
+        Account account = new Account(id, accountNumber, BigDecimal.valueOf(2000), user);
         return account;
     }
 }
